429906a86d01ef6024ed70942db8e910e7b8bb26
Shantanu-11 <shantanu857@gmail.com>
2021-03-13 22:16:20 +0530

    lec_05 final commit

--- lec04_webScrapping/activity/highest_wicket_taker.js
+++ lec04_webScrapping/activity/highest_wicket_taker.js
@@ -1,7 +1,6 @@
 let fs=require("fs");
 let cheerio=require("cheerio");
-const { request } = require("node:http");
-
+let request=require("request");
 request("https://www.espncricinfo.com/series/ipl-2020-21-1210595/delhi-capitals-vs-mumbai-indians-final-1237181/full-scorecard",cb);
 
 let highestWicketTaker={};
@@ -12,12 +11,29 @@ function parseData(html){
     let highestWicketTakenSoFar=0;
     let name;
     let economy;
+    let oppose
     let ch=cheerio.load(html);
     let bothBowlingTables = ch('.Collapsible .table.bowler');
     
     for(let i=0; i<bothBowlingTables.length;i++){
         let bowlingTable=bothBowlingTables[`${i}`];
+        let team=ch('.section-header.border-bottom.text-danger.cursor-pointer .header-title').text();
+        console.log(team);
         let allTrs=ch(bowlingTable).find("tbody tr");
-            for(let j=0;)
+            for(let j=0; j<allTrs.length;j++){
+                let allTds = ch(allTrs[j]).find("td");
+                let wicketsTaken=ch(allTds['4']).text();
+                if(wicketsTaken>highestWicketTakenSoFar){
+                    name=ch(allTds['0']).text();
+                    economy=ch(allTds['5']).text();
+                    highestWicketTakenSoFar=wicketsTaken;
+                }
+            }
     }
+    highestWicketTaker.name=name;
+    highestWicketTaker.wickets= highestWicketTakenSoFar;
+    highestWicketTaker.economy=economy;
+
+    console.log(highestWicketTaker);
+
 }
