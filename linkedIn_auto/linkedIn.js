317bbaf49ee3d4f8879ff81aa0a796db154c75b7
Shantanu-11 <shantanu857@gmail.com>
2021-04-11 17:26:30 +0530

    2nd commit

--- linkedIn_auto/linkedIn.js
+++ linkedIn_auto/linkedIn.js
@@ -1,21 +1,54 @@
 let puppeteer= require("puppeteer");
 let id="shantanu857@gmail.com";
-let password="Kittu@11";
+let pw="Kittu@11";
 
 (async function(){
     let browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: null,
-        args: ["--start-maximized"],
-      });
+        headless:false,
+        defaultViewport:null,
+        args:["--start-maximized"],
+    });
     let allPages = await browser.pages();
     let tab = allPages[0];
     await tab.goto("https://www.linkedin.com");
-    await tab.waitForSelector("#session_key");
+    await tab.waitForSelector('#session_key');
+    let inputs = await tab.$$('#session_key');
     await tab.type("#session_key",id);
-    await tab.type("#session_password",password);
+    await tab.type("#session_password",pw);
     await tab.click(".sign-in-form__submit-button");
-    await tab.waitForSelector('.search-global-typeahead.global-nav__search-typeahead');
+    await tab.waitForSelector('.search-global-typeahead.global-nav__search-typeahead',{visible:true});
     await tab.click('.search-global-typeahead.global-nav__search-typeahead');
     await tab.type('.search-global-typeahead.global-nav__search-typeahead',"google");
+    await tab.keyboard.press('Enter');
+    await tab.waitForSelector(".artdeco-pill.artdeco-pill--slate.artdeco-pill--2.artdeco-pill--choice.ember-view.search-reusables__filter-pill-button",{visible:true});
+    let peopleButton = await tab.$('.artdeco-pill.artdeco-pill--slate.artdeco-pill--2.artdeco-pill--choice.ember-view.search-reusables__filter-pill-button');
+    await peopleButton.click();
+    await tab.waitForSelector('.entity-result__title-text.t-16',{visible:true});
+    let connections = await tab.$$('.entity-result__title-text.t-16');
+    console.log(connections[0]);
+    await tab.waitForTimeout(5000);
+
+
+    for(let i=0;i<connections.length;i++){
+        let newTab = await browser.newPage();
+
+        await newTab.goto("https://www.google.com/")
+        await newTab.waitForTimeout(2000);
+        
+        let connect=connections[i];
+        let halfLink=await newTab.evaluate( function(elem){ return elem.getAttribute("href");  }  ,  connect);
+        console.log(halfLink);
+        let completeLink="https://www.linkedin.com"+halfLink;
+        
+        await newTab.goto(completeLink);
+
+        await newTab.waitForSelector('.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view',{visible:true});
+        await newTab.waitForTimeout(5000);
+        await newTab.click(completeLink);
+        await newTab.waitForTimeout(5000);
+        await newTab.waitForSelector('.ml1.artdeco-button.artdeco-button--3.artdeco-button--primary.ember-view',{visible:true});
+        await newTab.waitForTimeout(5000);
+        await newTab.click('.ml1.artdeco-button.artdeco-button--3.artdeco-button--primary.ember-view');  
+        await newTab.close();  
+    }
 })();
