4c5dd43ab0d2deba398c0e48e8d81504c7086e71
Shantanu <shantanu857@gmail.com>
2021-05-19 02:57:56 +0530

    incomplete project commit

---
+++ DOM/gira_ticket_mgmt/script.js
@@ -0,0 +1,16 @@
+let pink=document.querySelector(".pink");
+let blue=document.querySelector(".blue");
+let green=document.querySelector(".green");
+let black=document.querySelector(".black");
+pink.addEventListener('click',function(){
+    document.body.style.backgroundColor = "#ecf0f1";
+});
+blue.addEventListener('click',function(){
+    document.body.style.backgroundColor = "#2980b9";
+});
+green.addEventListener('click',function(){
+    document.body.style.backgroundColor = "#2ecc71";
+});
+black.addEventListener('click',function(){
+    document.body.style.backgroundColor = "#34495e";
+});
