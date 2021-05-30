b31bd46eed7f82669b067b628983aa36a4c89bb1
Shantanu-11 <shantanu857@gmail.com>
2021-05-30 06:20:02 +0530

    folders sort commit

--- tes2/Q2.js
+++
@@ -1,2 +0,0 @@
-  
-// Q - Convert the following function "f" to a pure function and create an impure function g which is a higher order function which take result of f and print it like f does it here.
